deafb744bc8ea1cd1f965816a0f437c0bed04dd7
effuone <effuone>
2022-08-07 14:13:47 +0600

    repositories

---
+++ Repositories/ProjectRepository.cs
@@ -0,0 +1,68 @@
+using Microsoft.EntityFrameworkCore;
+using TaskTracker.Api.Context;
+using TaskTracker.Api.Interfaces;
+using TaskTracker.Api.Models;
+
+namespace TaskTracker.Api.Repositories
+{
+    public class ProjectRepository : IProjectRepository
+    {
+        private readonly TaskContext _context;
+
+        public ProjectRepository(TaskContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddTaskToProject(int id, ToDoTask model)
+        {
+            var existingModel = await _context.Projects.FindAsync(id);
+            model.ProjectId = id;
+            existingModel.Tasks.Append(model);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> CreateAsync(Project model)
+        {
+            await _context.Projects.AddAsync(model);
+            await _context.SaveChangesAsync();
+            return model.ProjectId;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var model = await _context.Projects.FindAsync(id);
+            _context.Remove(model);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Project>> GetAllAsync()
+        {
+            return await _context.Projects.ToListAsync();
+        }
+
+        public async Task<Project> GetAsync(int id)
+        {
+            return await _context.Projects.FindAsync(id);
+        }
+
+        public async Task<Project> GetAsync(string name)
+        {
+            return await _context.Projects.Where(x=>x.ProjectName == name).FirstOrDefaultAsync();
+        }
+
+        public async Task RemoveTaskFromProject(int taskId)
+        {
+            var existingTask = await _context.Projects.FindAsync(taskId);
+            _context.Remove(existingTask);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(int id, Project model)
+        {
+            model.ProjectId = id;
+            _context.Update(model);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Repositories/TaskRepository.cs
@@ -0,0 +1,57 @@
+using Microsoft.EntityFrameworkCore;
+using TaskTracker.Api.Context;
+using TaskTracker.Api.Interfaces;
+using TaskTracker.Api.Models;
+
+namespace TaskTracker.Api.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly TaskContext _context;
+
+        public TaskRepository(TaskContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CreateAsync(ToDoTask model)
+        {
+            await _context.Tasks.AddAsync(model);
+            await _context.SaveChangesAsync();
+            return model.ToDoTaskId;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var model = await _context.Tasks.FindAsync(id);
+            _context.Remove(model);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<ToDoTask>> GetAllAsync()
+        {
+            return await _context.Tasks.ToListAsync();
+        }
+
+        public async Task<IEnumerable<ToDoTask>> GetAllAsync(int projectId)
+        {
+            return await _context.Tasks.Where(x=>x.ProjectId == projectId).ToListAsync();
+        }
+
+        public async Task<ToDoTask> GetAsync(int id)
+        {
+            return await _context.Tasks.FindAsync(id);
+        }
+
+        public async Task<ToDoTask> GetAsync(string name)
+        {
+            return await _context.Tasks.Where(x=>x.Name == name).FirstOrDefaultAsync();
+        }
+
+        public async Task UpdateAsync(int id, ToDoTask model)
+        {
+            model.ToDoTaskId = id;
+            _context.Update(model);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
